50e500a005dde073c772abc80fe5aa1408e3f14b
Kesova Elena <alyu@ukr.net>
2023-03-06 17:38:37 +0200

    Coordinates

---
+++ src/MyProject/Coordinates.java
@@ -0,0 +1,9 @@
+package MyProject;
+
+public class Coordinates {
+    public static void main(String[] args) {
+        double latitude = 46.482952;
+        double longitude = 30.712481;
+        System.out.println("Coordinates of Odessa is: Latitude - " + latitude + " Longitude - " + longitude);
+    }
+}
