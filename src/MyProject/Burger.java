45de775712b01c331fb33a80f566ae667b3e77eb
Kesova Elena <alyu@ukr.net>
2023-04-20 15:15:15 +0300

    retake Burger

--- src/MyProject/Burger.java
+++ src/MyProject/Burger.java
@@ -9,18 +9,18 @@ public class Burger {
 
 
     public Burger() {
-        System.out.println("Regular burger composition: " + this.bun +" "+ this.cheese +" "+ this.greens +" "+
-                this.meat +" "+ this.mayonnaise);
+        System.out.println("Regular burger composition: " + this.bun +", "+ this.cheese +", "+ this.greens +", "+
+                this.meat +", "+ this.mayonnaise);
     }
     public Burger(String meat) {
         this.meat = meat;
-        System.out.println("Double meat burger composition: " + this.bun +" "+ this.cheese +" "+ this.greens +" "+
-                this.meat +" "+ this.mayonnaise);
+        System.out.println("Double meat burger composition: " + this.bun +", "+ this.cheese +", "+ this.greens +", "+
+                this.meat +", "+ this.mayonnaise);
     }
     public Burger(String meat, String mayonnaise) {
         this.meat = meat;
         this.mayonnaise = mayonnaise;
-        System.out.println("Diet burger composition: " + this.bun +" "+ this.cheese +" "+ this.greens +" "+
-                this.meat);
+        System.out.println("Diet burger composition: " + this.bun +", "+ this.cheese +", "+ this.greens +", "+
+                this.meat +", "+ this.mayonnaise);
     }
 }
